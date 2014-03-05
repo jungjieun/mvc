eb7bb0efdddd5acfbbce25c6a436c624d151da63
jungjieun <jje0000@naver.com>
2014-03-05 13:27:06 +0900

    라우트 지정하기

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Mvc::Application.routes.draw do
-  get 'home/hello' => 'home#hello'
+  get 'home/hello' => 'home#hello' 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
